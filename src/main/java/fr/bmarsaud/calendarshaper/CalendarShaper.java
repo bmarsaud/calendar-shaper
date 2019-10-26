10046d4662eccb758d7312b36079dce7f5aa6855
Bastien Marsaud <bastien@mouseover.fr>
2019-10-26 16:38:29 +0200

    feat: creates endpoint for each calendar

--- src/main/java/fr/bmarsaud/calendarshaper/CalendarShaper.java
+++ src/main/java/fr/bmarsaud/calendarshaper/CalendarShaper.java
@@ -3,18 +3,37 @@ package fr.bmarsaud.calendarshaper;
 import org.glassfish.grizzly.http.server.HttpServer;
 
 import java.io.IOException;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.util.ArrayList;
 
 import fr.bmarsaud.calendarshaper.http.RequestHandler;
+import fr.bmarsaud.calendarshaper.model.Calendar;
 
 public class CalendarShaper {
-    public static void main(String[] args) {
-        int port = 8080;
-        if(args.length == 1) {
-            port = Integer.valueOf(args[0]);
+    private ArrayList<Calendar> calendars;
+
+    public CalendarShaper() {
+        calendars = new ArrayList<>();
+    }
+
+    public void loadCalendars() {
+        URL url = null;
+        try {
+            url = new URL("http://example.com/calendar.ics");
+        } catch (MalformedURLException e) {
+            e.printStackTrace();
         }
 
+        calendars.add(new Calendar("test", url));
+    }
+
+    public void start(int port) {
         HttpServer server = HttpServer.createSimpleServer("", port);
-        server.getServerConfiguration().addHttpHandler(new RequestHandler(), "/test");
+
+        for(Calendar calendar : calendars) {
+            server.getServerConfiguration().addHttpHandler(new RequestHandler(calendar), "/" + calendar.getName());
+        }
 
         try {
             server.start();
@@ -23,5 +42,16 @@ public class CalendarShaper {
         } catch (IOException e) {
             e.printStackTrace();
         }
+    }
+
+    public static void main(String[] args) {
+        int port = 8080;
+        if(args.length == 1) {
+            port = Integer.valueOf(args[0]);
+        }
+
+        CalendarShaper calendarShaper = new CalendarShaper();
+        calendarShaper.loadCalendars();
+        calendarShaper.start(port);
     }
 }
--- src/main/java/fr/bmarsaud/calendarshaper/http/RequestHandler.java
+++ src/main/java/fr/bmarsaud/calendarshaper/http/RequestHandler.java
@@ -4,11 +4,19 @@ import org.glassfish.grizzly.http.server.HttpHandler;
 import org.glassfish.grizzly.http.server.Request;
 import org.glassfish.grizzly.http.server.Response;
 
+import fr.bmarsaud.calendarshaper.model.Calendar;
+
 public class RequestHandler extends HttpHandler {
+    private Calendar calendar;
+
+    public RequestHandler(Calendar calendar) {
+        this.calendar = calendar;
+    }
+
     @Override
     public void service(Request request, Response response) throws Exception {
         //TODO: forwards request
-        final String message = "It works !";
+        final String message = "Calendar \"" + calendar.getName() + "\"";
         response.setContentType("text/plain");
         response.setContentLength(message.length());
         response.getWriter().write(message);
