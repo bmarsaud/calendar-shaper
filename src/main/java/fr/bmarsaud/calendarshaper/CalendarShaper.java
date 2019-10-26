41d5f4137dcff6e0c5cd27e50068141ba6be376d
Bastien Marsaud <bastien@mouseover.fr>
2019-10-26 22:31:09 +0200

    feat: adds calendar load and save

--- src/main/java/fr/bmarsaud/calendarshaper/CalendarShaper.java
+++ src/main/java/fr/bmarsaud/calendarshaper/CalendarShaper.java
@@ -1,31 +1,58 @@
 package fr.bmarsaud.calendarshaper;
 
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+
 import org.glassfish.grizzly.http.server.HttpServer;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.FileWriter;
 import java.io.IOException;
-import java.net.MalformedURLException;
-import java.net.URL;
 import java.util.ArrayList;
+import java.util.Arrays;
 
 import fr.bmarsaud.calendarshaper.http.RequestHandler;
 import fr.bmarsaud.calendarshaper.model.Calendar;
+import fr.bmarsaud.calendarshaper.model.rules.CalendarRule;
+import fr.bmarsaud.calendarshaper.model.rules.RuleSerializer;
 
 public class CalendarShaper {
     private ArrayList<Calendar> calendars;
+    private Gson gson;
 
     public CalendarShaper() {
         calendars = new ArrayList<>();
+
+        GsonBuilder gsonBuilder = new GsonBuilder();
+        gsonBuilder.registerTypeAdapter(CalendarRule.class, new RuleSerializer());
+        gson = gsonBuilder.setPrettyPrinting().create();
     }
 
     public void loadCalendars() {
-        URL url = null;
+        File file = new File("calendars.json");
+
+        if(file.exists()) {
+            try {
+                FileReader fileReader = new FileReader(file);
+                calendars = new ArrayList(Arrays.asList(gson.fromJson(fileReader, Calendar[].class)));
+            } catch (FileNotFoundException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+    public void saveCalendars() {
+        File file = new File("calendars.json");
         try {
-            url = new URL("http://example.com/calendar.ics");
-        } catch (MalformedURLException e) {
+            FileWriter fileWriter = new FileWriter(file);
+            fileWriter.write(gson.toJson(calendars));
+            fileWriter.flush();
+            fileWriter.close();
+        } catch (IOException e) {
             e.printStackTrace();
         }
-
-        calendars.add(new Calendar("test", url));
     }
 
     public void start(int port) {
@@ -42,6 +69,8 @@ public class CalendarShaper {
         } catch (IOException e) {
             e.printStackTrace();
         }
+
+        saveCalendars();
     }
 
     public static void main(String[] args) {
